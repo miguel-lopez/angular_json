2a53653acb8016a1f21cadc50c59c6c58325a4ca
Mike LC <miguelopcor20@gmail.com>
2016-09-08 17:17:17 -0500

    Cambia goles, pero hay un error con las variables de marcador_ local_temp y la otra. Muestra la alerta cada que cambias el json.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,43 @@
 var app = angular.module("app", ["ngResource"]);
 
 
-app.controller('appController',function($scope, $http) {
+app.controller('appController', function ($scope, $http) {
 
-  // Function to get the data
-  $scope.getData = function(){
-    $http.get('js/data.json')
-      .success(function(data, status, headers, config) {
-          $scope.datos = data;
-          
-        console.log('Fetched data!');
-      });
-  };
+    // Function to get the data
+    var marcador_local_temp = 0;
+    var marcador_visitante_temp = 0;
+    $scope.getData = function () {
+        $http.get('js/data.json')
+                .success(function (data, status, headers, config) {
+                    $scope.datos = data;
+                    $scope.result = [
+                        angular.fromJson(data[0]),
+                        angular.fromJson(data[1])
+                    ];
+                    //console.log("RESULT",$scope.result[0]);
 
-  // Run function every second
-  setInterval($scope.getData, 1000);
+                    $scope.partidos = $scope.result[0].partidos;
+                    angular.forEach($scope.partidos, function (value, key) {
+                        $scope.idPartido = value.id;
+                        if (value.status === "jugando") {
+                            var marcador_local = value.marcador_local;
+                            var marcador_visitante = value.marcador_visitante;
+                            if (marcador_local > marcador_local_temp) {
+                                alert("¡GOOOOOOL DE: "+ value.equipo_local);
+                                marcador_local_temp = marcador_local;
+                            }
+                            if (marcador_visitante > marcador_visitante_temp) {
+                                alert("¡GOOOOOOL DE: "+ value.equipo_visitante);
+                                marcador_visitante_temp = marcador_visitante;
+                            }
+                        }
+                    });
+
+                    console.log('Fetched data!');
+                });
+    };
+
+    // Run function every second
+    setInterval($scope.getData, 1000);
 
 });
