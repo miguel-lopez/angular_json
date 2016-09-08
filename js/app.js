ea4607afa2e5d8dcd270b170f0bb394f50e40dc6
Mike LC <miguelopcor20@gmail.com>
2016-09-08 13:01:43 -0500

    Lee json cada segundo

--- js/app.js
+++ js/app.js
@@ -1,25 +1,19 @@
-
-//para hacer uso de $resource debemos colocarlo al crear el modulo
 var app = angular.module("app", ["ngResource"]);
- 
-//con dataResource inyectamos la factoría
-app.controller("appController", function ($scope, $http, dataResource) {
-    //hacemos uso de $http para obtener los datos del json
-    $http.get('js/data.json').success(function (data) {
-        //Convert data to array.
-        //datos lo tenemos disponible en la vista gracias a $scope
-        $scope.datos = data;
-    });
-    //datosResource lo tenemos disponible en la vista gracias a $scope
-    $scope.datosResource = dataResource.get();
-})
- 
-//de esta forma tan sencilla consumimos con $resource en AngularJS
-app.factory("dataResource", function ($resource) {
-    return $resource("js/data.json", //la url donde queremos consumir
-        {}, //aquí podemos pasar variables que queramos pasar a la consulta
-        //a la función get le decimos el método, y, si es un array lo que devuelve
-        //ponemos isArray en true
-        { get: { method: "GET", isArray: true }
-    })
-})
+
+
+app.controller('appController',function($scope, $http) {
+
+  // Function to get the data
+  $scope.getData = function(){
+    $http.get('js/data.json')
+      .success(function(data, status, headers, config) {
+          $scope.datos = data;
+          
+        console.log('Fetched data!');
+      });
+  };
+
+  // Run function every second
+  setInterval($scope.getData, 1000);
+
+});
